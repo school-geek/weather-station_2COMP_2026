9b8b238b61922a4840b65f3fc258dd9660b7a36b
Codingeek45 <felix.james.ashton.love@gmail.com>
2026-03-12 12:57:34 +1300

    add comments and more info to logger funcs

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,48 @@
 #include <Arduino.h>
 
-//int myFunction(int, int);
-
 void setup() {
-
+  // initialize serial communication at 115200 bits per second:
 Serial.begin(115200);
-
-
 }
 
 void loop() {
   Serial.println("Hello, world!");
 }
 
-// Built in library: functions
+/* 
+
+Built in library: functions
+
+*/
+// read sensor data on provided pin and return the value
+int readSensorData(byte pin)
+{
+    // reading sensor data
+    int sensorValue = analogRead(pin);
+    deubgOutputINT("Sensor", sensorValue);
+    return sensorValue;
+}
+
+// print given debug information to the serial monitor | STRING
+// debugOutputSTR("start of loop") will print: [DEBUG] start of loop
 void deubgOutputSTR(String str)
 {
     Serial.print("[DEBUG] ");
     Serial.println(str);
 }
 
+// print given debug information to the serial monitor | INT
+// debugOutputINT("Temp", 25) will print: [DEBUG] Temp: 25
 void deubgOutputINT(String sensor, int num)
 {
-    Serial.print("[DEBUG ");
+    Serial.print("[DEBUG] ");
     Serial.print(sensor);
     Serial.print(": ");
     Serial.println(num);
 }
 
+// print given information to the serial monitor | STRING
+// infoOutput("start of loop") will print: [INFO] start of loop
 void infoOutput(String str)
 {
     Serial.print("[INFO] ");
