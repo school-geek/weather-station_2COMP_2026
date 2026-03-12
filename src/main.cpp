74bb376c435d53131591ab3635eea415b1e8dcaa
Codingeek45 <felix.james.ashton.love@gmail.com>
2026-03-12 20:54:21 +1300

    added base data processing func

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,38 @@ void loop() {
 Built in library: functions
 
 */
+
+// basic funcs
+
+// print given debug information to the serial monitor | STRING
+// debugOutputSTR("start of loop") will print: [DEBUG] start of loop
+void debugOutputSTR(String data)
+{
+  if (!DEBUG) return; // skip debug output if DEBUG is false
+  Serial.print("[DEBUG] ");
+  Serial.println(data);
+}
+
+// print given debug information to the serial monitor | INT
+// debugOutputINT("Temp", 25) will print: [DEBUG] Temp: 25
+void debugOutputINT(String sensor, int data)
+{
+  if (!DEBUG) return; // skip debug output if DEBUG is false
+  Serial.print("[DEBUG] ");
+  Serial.print(sensor);
+  Serial.print(": ");
+  Serial.println(data);
+}
+
 // read sensor data on provided pin and return the value
-int readSensorData(byte pin)
+int readSensorData(String sensor, byte pin)
 {
   // reading sensor data
   int sensorValue = analogRead(pin);
-  debugOutputINT("Sensor", sensorValue);
+  if (DEBUG)
+  {
+    debugOutputINT(sensor, sensorValue);
+  }
   return sensorValue;
 }
 
@@ -33,28 +59,29 @@ void infoOutput(String str)
   Serial.println(str);
 }
 
+// data processing funcs
 
-//
-// DEBUB funcs
-//
-
-
-// print given debug information to the serial monitor | STRING
-// debugOutputSTR("start of loop") will print: [DEBUG] start of loop
-void debugOutputSTR(String str)
+// process raw sensor data and return a formatted string
+// processData(25, "Temp") will return "25 °C"
+String processData(int sensorValue, String sensor)
 {
-  if (!DEBUG) return; // skip debug output if DEBUG is false
-  Serial.print("[DEBUG] ");
-  Serial.println(str);
-}
-
-// print given debug information to the serial monitor | INT
-// debugOutputINT("Temp", 25) will print: [DEBUG] Temp: 25
-void debugOutputINT(String sensor, int num)
-{
-  if (!DEBUG) return; // skip debug output if DEBUG is false
-  Serial.print("[DEBUG] ");
-  Serial.print(sensor);
-  Serial.print(": ");
-  Serial.println(num);
+  String processedData = ""; // initialize processedData as an empty string
+ 
+  if (sensor == "Temp")
+  {
+    // sensorValue = map(sensorValue, 000, 000, 000, 000);
+    processedData = String(sensorValue) + " °C";
+  }
+  /*
+  else if (sensor == "Light")
+  {
+    // sensorValue = map(sensorValue, 000, 000, 000, 000);
+    processedData = String(sensorValue) + " %";
+  }
+  */
+  if (DEBUG)
+  {
+    debugOutputSTR("Processed data: " + processedData);
+  }
+  return processedData;
 }
