f48faa12bf84a16d9ef88d883e77e533aec57809
Codingeek45 <felix.james.ashton.love@gmail.com>
2026-03-12 13:48:43 +1300

    added more comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 
+bool DEBUG = true;
+
 void setup() {
-  // initialize serial communication at 115200 bits per second:
+// initialize serial communication at 115200 bits per second:
 Serial.begin(115200);
 }
 
@@ -17,34 +19,42 @@ Built in library: functions
 // read sensor data on provided pin and return the value
 int readSensorData(byte pin)
 {
-    // reading sensor data
-    int sensorValue = analogRead(pin);
-    deubgOutputINT("Sensor", sensorValue);
-    return sensorValue;
-}
-
-// print given debug information to the serial monitor | STRING
-// debugOutputSTR("start of loop") will print: [DEBUG] start of loop
-void deubgOutputSTR(String str)
-{
-    Serial.print("[DEBUG] ");
-    Serial.println(str);
-}
-
-// print given debug information to the serial monitor | INT
-// debugOutputINT("Temp", 25) will print: [DEBUG] Temp: 25
-void deubgOutputINT(String sensor, int num)
-{
-    Serial.print("[DEBUG] ");
-    Serial.print(sensor);
-    Serial.print(": ");
-    Serial.println(num);
+  // reading sensor data
+  int sensorValue = analogRead(pin);
+  debugOutputINT("Sensor", sensorValue);
+  return sensorValue;
 }
 
 // print given information to the serial monitor | STRING
 // infoOutput("start of loop") will print: [INFO] start of loop
 void infoOutput(String str)
 {
-    Serial.print("[INFO] ");
-    Serial.println(str);
+  Serial.print("[INFO] ");
+  Serial.println(str);
+}
+
+
+//
+// DEBUB funcs
+//
+
+
+// print given debug information to the serial monitor | STRING
+// debugOutputSTR("start of loop") will print: [DEBUG] start of loop
+void debugOutputSTR(String str)
+{
+  if (!DEBUG) return; // skip debug output if DEBUG is false
+  Serial.print("[DEBUG] ");
+  Serial.println(str);
+}
+
+// print given debug information to the serial monitor | INT
+// debugOutputINT("Temp", 25) will print: [DEBUG] Temp: 25
+void debugOutputINT(String sensor, int num)
+{
+  if (!DEBUG) return; // skip debug output if DEBUG is false
+  Serial.print("[DEBUG] ");
+  Serial.print(sensor);
+  Serial.print(": ");
+  Serial.println(num);
 }
